8592af8ec68ede44b00ae84c81b86d7019628939
dante rené sack <sack_dante@hotmail.com>
2022-01-04 22:22:12 -0300

    add apollo server with mock data

--- server/src/schema.js
+++ server/src/schema.js
@@ -3,18 +3,27 @@ const { gql } = require('apollo-server');
 const typeDefs = gql`
   "A Track is a group of Modules that teaches about a specific topic."
   type Track {
+    "The track's unique ID."
     id: ID!
+    "The track's title."
     title: String!
+    "The track's main author."
     author: Author!
+    "The track's main illustration to display in track card or track page detail."
     thumbnail: String
-    lenght: Int
+    "The track's approximate length to complete, in minutes."
+    length: Int
+    "The number of modules this track contains."
     modulesCount: Int
   }
 
   "Author of a complete Track or a Module."
   type Author {
+    "The author's unique ID."
     id: ID!
+    "Author's first and last name."
     name: String!
+    "Author's profile picture url."
     photo: String
   }
 
